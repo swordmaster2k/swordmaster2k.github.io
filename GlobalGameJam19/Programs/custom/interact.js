e1bfc2536a6a48821c2f403437ee43db1f2b4529
Joshua Michael Daly <joshua.michael.daly@gmail.com>
2019-03-05 19:06:40 +0000

    Replaced individual item programs with single one, compressed images to save space

--- GlobalGameJam19/Programs/custom/interact.js
+++ GlobalGameJam19/Programs/custom/interact.js
@@ -1,24 +1,46 @@
 // We are interacting with something.
 rpgcode.setGlobal("interacting", true);
 
-// Play the correct animation based on the direction of the character.
-const characterId = "guy.character";
-const direction = rpgcode.getCharacterDirection();
-if (direction === "WEST" || direction === "NORTH") {
-   rpgcode.animateCharacter(characterId, "INTERACT_WEST", finish);
-} else {
-   rpgcode.animateCharacter(characterId, "INTERACT_EAST", finish);
+// Get the sprite ID either from a vector that triggered this, or from the sprite that triggered this.
+var spriteId = "";
+if (this.vectorId) {
+   // Storing target ID on the vector itself.
+   spriteId = this.vectorId;
+} else if (this.sprite) {
+   // Triggered straight from the sprite.
+   spriteId = this.sprite.id;
 }
 
-/*
- * Called when the interaction animation is finished. 
- */
-function finish() {
-   // Give the character the item, destroying it.
-   rpgcode.destroySprite("item_1");
-   var i = rpgcode.getGlobal("items");
-   rpgcode.setGlobal("items", ++i);
-   rpgcode.setGlobal("interacting", false);
+const characterId = "guy.character";
+if (rpgcode.getSprite(spriteId)) {
+   // Play the directional grab animation.
+   const direction = rpgcode.getCharacterDirection();
+   if (direction === "WEST" || direction === "NORTH") {
+      rpgcode.animateCharacter(characterId, "INTERACT_WEST", finish);
+   } else {
+      rpgcode.animateCharacter(characterId, "INTERACT_EAST", finish);
+   }
+} else {
    rpgcode.resetActivationChecks(characterId);
    rpgcode.endProgram();
+}
+
+function finish() {
+   try {
+      // Destroy the sprite on the board, and increment the number of player items.
+      rpgcode.destroySprite(spriteId);
+      var i = rpgcode.getGlobal("items");
+      if (i < 5) {
+         rpgcode.setGlobal("items", ++i);
+         rpgcode.setGlobal("interacting", false);
+         rpgwizard.craftyBoard.board.layers[2].images[1].src = "ui/counter_" + (i+1) + ".png";
+      }
+
+      rpgcode.resetActivationChecks(characterId);
+      rpgcode.endProgram();
+   } catch (ex) {
+      console.log(ex);
+      rpgcode.resetActivationChecks(characterId);
+      rpgcode.endProgram();
+   }
 }
